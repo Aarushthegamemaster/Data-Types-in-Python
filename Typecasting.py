204df24ae5bbc5beba30a425b7cd0dd92019d1b0
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-02-15 17:49:01 -0800

    Typecasting_is_fun

---
+++ Typecasting.py
@@ -0,0 +1,23 @@
+name = "Aarush"
+age = 11
+School = "Mesa Verde Middle School"
+boolean = True or False
+
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(School)
+print(type(School))
+print(boolean)
+print(type(boolean))
+
+print("after typecasting")
+age = str(age)
+print(type(age))
+name = str(name)
+print(type(name))
+School = str(School) 
+print(type(School))
+boolean = bool(boolean)
+print(type(boolean))

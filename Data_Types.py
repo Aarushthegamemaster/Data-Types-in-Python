a87917ea7fa3d30a761c78f307a6f389ba6d8f8b
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-02-15 17:31:24 -0800

    Data_Types

---
+++ Data_Types.py
@@ -0,0 +1,9 @@
+a = 8.99
+b = 9
+c = "Aarush Chandra"
+d = True or False
+print("Variable", a,"type is",type(a))
+print("Variable", b,"type is",type(b))
+print("Variable", c,"type is",type(c))
+print("Variable", d,"type is",type(d))
+
